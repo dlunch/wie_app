a3624976b33ba72e0ac80bb0361278e34c370bc9
Inseok Lee <git@dlun.ch>
2024-05-06 14:02:10 +0900

    midi out

--- wie_web/src/rust/audio_sink.rs
+++ wie_web/src/rust/audio_sink.rs
@@ -1,8 +1,9 @@
 use midir::MidiOutputConnection;
 use rodio::{buffer::SamplesBuffer, OutputStreamHandle, Sink};
+use spin::Mutex;
 
 pub struct AudioSink {
-    _midi_out: MidiOutputConnection,
+    midi_out: Mutex<MidiOutputConnection>,
     sink: Sink,
 }
 
@@ -13,7 +14,10 @@ unsafe impl Send for AudioSink {}
 impl AudioSink {
     pub fn new(midi_out: MidiOutputConnection, stream_handle: &OutputStreamHandle) -> Self {
         let sink = Sink::try_new(stream_handle).unwrap();
-        Self { _midi_out: midi_out, sink }
+        Self {
+            midi_out: Mutex::new(midi_out),
+            sink,
+        }
     }
 }
 
@@ -24,19 +28,19 @@ impl wie_backend::AudioSink for AudioSink {
         self.sink.append(buffer);
     }
 
-    fn midi_note_on(&self, _channel_id: u8, _note: u8, _velocity: u8) {
-        // TODO
+    fn midi_note_on(&self, channel_id: u8, note: u8, velocity: u8) {
+        self.midi_out.lock().send(&[0x90 | channel_id, note, velocity]).unwrap();
     }
 
-    fn midi_note_off(&self, _channel_id: u8, _note: u8, _velocity: u8) {
-        // TODO
+    fn midi_note_off(&self, channel_id: u8, note: u8, velocity: u8) {
+        self.midi_out.lock().send(&[0x80 | channel_id, note, velocity]).unwrap();
     }
 
-    fn midi_program_change(&self, _channel_id: u8, _program: u8) {
-        // TODO
+    fn midi_control_change(&self, channel_id: u8, control: u8, value: u8) {
+        self.midi_out.lock().send(&[0xB0 | channel_id, control, value]).unwrap()
     }
 
-    fn midi_control_change(&self, _channel_id: u8, _control: u8, _value: u8) {
-        // TODOs
+    fn midi_program_change(&self, channel_id: u8, program: u8) {
+        self.midi_out.lock().send(&[0xC0 | channel_id, program]).unwrap()
     }
 }
